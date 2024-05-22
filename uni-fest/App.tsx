563deb816ec4168223a9aee5db65363711e2f280
krhuhun <jorgelisboa659@gmail.com>
2024-05-22 09:18:09 -0300

    feat: added new dependencies, stack navigation for Login screen

--- uni-fest/App.tsx
+++ uni-fest/App.tsx
@@ -1,15 +1,19 @@
+import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { PaperProvider } from "react-native-paper";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./src/screens/Login";
 
+const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <PaperProvider>
-        
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
       </PaperProvider>
     </NavigationContainer>
   );
 }
-
